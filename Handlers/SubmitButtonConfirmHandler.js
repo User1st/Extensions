454af14c9fc3ac138de58fd9b4dc771e9b0646b8
dmitryuser1st <dmitry.melnikov@user1st.com>
2016-11-14 14:55:11 +0200

    SubmitButtonConfirmHandler is updated

--- Handlers/SubmitButtonConfirmHandler.js
+++ Handlers/SubmitButtonConfirmHandler.js
@@ -1,8 +1,49 @@
-﻿(function (ns, $) {
+﻿(function (ns, $)
+{
     "use strict";
 
-    //ns.Extesion.Handlers namespace hast be used
+    if (ns.Extensions.Handlers.SubmitButtonConfirmHandler)
+        return;
 
 
+    ns.Extensions.Handlers.SubmitButtonConfirmHandler = function ()
+    {
+        var _init = function ()
+        {
+            $("input[type='submit']").each(function ()
+            {
+                $(this).bind("click", function(ev){
+					var r = confirm("Do you really want to submit the form?");
+					if(!r)
+					{
+						ev.preventDefault();
+						ev.stopPropagation();
+						return false;
+					}
+				})
+            });
+        },
+        handler = {
+            handle: function ()
+            {
+                _init();
+            },
+            dispose: function ()
+            {
+
+            }
+        };
+
+        return handler;
+    };
+
+    ns.Extensions.Extesion.addHandler("SubmitButtonConfirmHandler",
+                    ns.handlerTarget.page,
+                    20000,
+                    null,
+                    null,
+                    ns.Extensions.Handlers.SubmitButtonConfirmHandler.handle,
+                    ns.Extensions.Handlers.SubmitButtonConfirmHandler.dispose,
+                    null);
 
 })(User1st.Web, uf$);
